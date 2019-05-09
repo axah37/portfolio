eca09e5fe65d7954d2b1f3f93c888e52b5fd697a
Abdurahman Ahmed <abdahmed@live.ca>
2019-05-09 17:28:07 -0500

    Added Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	image = models.ImageField(upload_to='images/')
+	title = models.CharField(max_length = 140)
+	body = models.TextField()
+	date = models.DateTimeField()
+
+	def __str__(self):
+		return self.title
