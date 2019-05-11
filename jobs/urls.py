587e7c2d25196eeaafa1cd3b1df590af070ca7c7
Abdurahman Ahmed <abdahmed@live.ca>
2019-05-10 20:32:46 -0500

    correct jobs url error

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 import jobs.views
 
 urlpatterns = [
-	path('jobs/', jobs.views.summary, name='jobs_home'),
+	path('', jobs.views.summary, name='jobs_home'),
     path('<int:jobs_id>', jobs.views.detail, name='jobs_detail'),
 ]
